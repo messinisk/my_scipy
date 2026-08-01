46388df35bc6b25906967a8c8541d7c69b76cc19
messinisk <messiniskostas0@gmail.com>
2026-08-02 00:33:39 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scipy-analytics",
+    name="scipy_analytics",
     version="0.1.1",
     description="Extensions for SciPy: Pearson distributions, Monte Carlo, fitting, plotting",
     author="messinisk",
